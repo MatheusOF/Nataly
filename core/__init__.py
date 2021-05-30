acfb7333c617c9801ddd17b3f8338a3279bcbc2e
MatheusOF <matheusnivo5@gmail.com>
2021-05-30 10:46:09 -0300

    0.3
    Prestar atençao no buffer do pyaudio
    
    2048

---
+++ core/__init__.py
@@ -0,0 +1,15 @@
+# Comando para falar as horas;
+
+import datetime
+
+class SystemInfo:
+    def __init__(self):
+        pass
+
+
+    @staticmethod
+    def get_time():
+        now = datetime.datetime.now()
+        answer = 'São {} horas e {} minutos.'.format(now.hour, now.minute)
+        return answer
+
